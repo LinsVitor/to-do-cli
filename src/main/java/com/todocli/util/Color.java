40b0f4cf579789d917ad2fe29df99ccf7d729d71
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-25 13:47:49 -0300

    feat(util): Cria classe Color para formatação de console
    - Implementa constantes ANSI para colorir saídas no terminal

---
+++ src/main/java/com/todocli/util/Color.java
@@ -0,0 +1,173 @@
+package com.todocli.util;
+
+public class Color {
+
+    public static String RESET ="\u001B[0m";
+    public static String  BLACK  = "\u001B[30m";
+    public static String  RED    = "\u001B[31m";
+    public static String  GREEN  = "\u001B[32m";
+    public static String  YELLOW = "\u001B[33m";
+    public static String  BLUE   = "\u001B[34m";
+    public static String  PURPLE = "\u001B[35m";
+    public static String  CYAN   = "\u001B[36m";
+    public static String  WHITE  = "\u001B[37m";
+
+    public static String  BRIGHT_BLACK  = "\u001B[90m";
+    public static String  BRIGHT_RED    = "\u001B[91m";
+    public static String  BRIGHT_GREEN  = "\u001B[92m";
+    public static String  BRIGHT_YELLOW = "\u001B[93m";
+    public static String  BRIGHT_BLUE   = "\u001B[94m";
+    public static String  BRIGHT_PURPLE = "\u001B[95m";
+    public static String  BRIGHT_CYAN   = "\u001B[96m";
+    public static String  BRIGHT_WHITE  = "\u001B[97m";
+
+    public static String  BG_BLACK  = "\u001B[40m";
+    public static String  BG_RED    = "\u001B[41m";
+    public static String  BG_GREEN  = "\u001B[42m";
+    public static String  BG_YELLOW = "\u001B[43m";
+    public static String  BG_BLUE   = "\u001B[44m";
+    public static String  BG_PURPLE = "\u001B[45m";
+    public static String  BG_CYAN   = "\u001B[46m";
+    public static String  BG_WHITE  = "\u001B[47m";
+
+    public static String  BRIGHT_BG_BLACK  = "\u001B[100m";
+    public static String  BRIGHT_BG_RED    = "\u001B[101m";
+    public static String  BRIGHT_BG_GREEN  = "\u001B[102m";
+    public static String  BRIGHT_BG_YELLOW = "\u001B[103m";
+    public static String  BRIGHT_BG_BLUE   = "\u001B[104m";
+    public static String  BRIGHT_BG_PURPLE = "\u001B[105m";
+    public static String  BRIGHT_BG_CYAN   = "\u001B[106m";
+    public static String  BRIGHT_BG_WHITE  = "\u001B[107m";
+
+    public static String getRESET() {
+        return RESET;
+    }
+
+    public static String getBLACK() {
+        return BLACK;
+    }
+
+    public static String getRED() {
+        return RED;
+    }
+
+    public static String getGREEN() {
+        return GREEN;
+    }
+
+    public static String getYELLOW() {
+        return YELLOW;
+    }
+
+    public static String getBLUE() {
+        return BLUE;
+    }
+
+    public static String getPURPLE() {
+        return PURPLE;
+    }
+
+    public static String getCYAN() {
+        return CYAN;
+    }
+
+    public static String getWHITE() {
+        return WHITE;
+    }
+
+    public static String getBrightBlack() {
+        return BRIGHT_BLACK;
+    }
+
+    public static String getBrightRed() {
+        return BRIGHT_RED;
+    }
+
+    public static String getBrightGreen() {
+        return BRIGHT_GREEN;
+    }
+
+    public static String getBrightYellow() {
+        return BRIGHT_YELLOW;
+    }
+
+    public static String getBrightBlue() {
+        return BRIGHT_BLUE;
+    }
+
+    public static String getBrightPurple() {
+        return BRIGHT_PURPLE;
+    }
+
+    public static String getBrightCyan() {
+        return BRIGHT_CYAN;
+    }
+
+    public static String getBrightWhite() {
+        return BRIGHT_WHITE;
+    }
+
+    public static String getBgBlack() {
+        return BG_BLACK;
+    }
+
+    public static String getBgRed() {
+        return BG_RED;
+    }
+
+    public static String getBgGreen() {
+        return BG_GREEN;
+    }
+
+    public static String getBgYellow() {
+        return BG_YELLOW;
+    }
+
+    public static String getBgBlue() {
+        return BG_BLUE;
+    }
+
+    public static String getBgPurple() {
+        return BG_PURPLE;
+    }
+
+    public static String getBgCyan() {
+        return BG_CYAN;
+    }
+
+    public static String getBgWhite() {
+        return BG_WHITE;
+    }
+
+    public static String getBrightBgBlack() {
+        return BRIGHT_BG_BLACK;
+    }
+
+    public static String getBrightBgRed() {
+        return BRIGHT_BG_RED;
+    }
+
+    public static String getBrightBgGreen() {
+        return BRIGHT_BG_GREEN;
+    }
+
+    public static String getBrightBgYellow() {
+        return BRIGHT_BG_YELLOW;
+    }
+
+    public static String getBrightBgBlue() {
+        return BRIGHT_BG_BLUE;
+    }
+
+    public static String getBrightBgPurple() {
+        return BRIGHT_BG_PURPLE;
+    }
+
+    public static String getBrightBgCyan() {
+        return BRIGHT_BG_CYAN;
+    }
+
+    public static String getBrightBgWhite() {
+        return BRIGHT_BG_WHITE;
+    }
+}
