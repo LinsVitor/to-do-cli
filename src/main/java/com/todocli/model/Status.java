e48c1e69e11bc59e4d5eb2cc8048918b8f922441
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-28 23:00:32 -0300

    refactor(model): Delega responsabilidade ao enum Status
    - Adiciona método fromString para conversão e validação
    - Lança InvalidArgumentException para valores inválidos
    - Remove lógica de validação de outras classes

--- src/main/java/com/todocli/model/Status.java
+++ src/main/java/com/todocli/model/Status.java
@@ -1,7 +1,26 @@
 package com.todocli.model;
 
+import com.todocli.exception.InvalidArgumentException;
+import com.todocli.util.ErrorMessage;
+
+import java.util.Arrays;
+
 public enum Status {
     TODO,
     IN_PROGRESS,
     DONE;
+
+    public static Status fromString(String value) {
+        if (value.trim().isEmpty()) {
+            throw new InvalidArgumentException("Status " + ErrorMessage.EMPTY_FIELD.getMessage());
+        }
+        if (value.equalsIgnoreCase("null")) {
+            throw new InvalidArgumentException("Status " + ErrorMessage.NULL_FIELD.getMessage());
+        }
+        String validValue = value.replaceFirst(" ", "_").replace("-", "_").trim().toUpperCase();
+        if (Arrays.stream(Status.values()).noneMatch(status -> status.name().equals(validValue))) {
+            throw new InvalidArgumentException(ErrorMessage.NO_MATCH_STATUS.getMessage());
+        }
+        return Status.valueOf(validValue);
+    }
 }
