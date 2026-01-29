96882b0f68b854901eaa4ec4ff54acb2e7c70c73
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-28 23:11:55 -0300

    chore: Renomeia todo-cli.jar para to-do-cli.jar
    - Corrige nome para formato padrão com hífen

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 CLS
-javac -d out src/main/java/com/todocli/*.java src/main/java/com/todocli/cli/*.java src/main/java/com/todocli/db/*.java src/main/java/com/todocli/model/*.java src/main/java/com/todocli/repository/*.java src/main/java/com/todocli/service/*.java src/main/java/com/todocli/util/*.java
-jar cvfm todo-cli.jar MANIFEST.MF -C out .
+javac -d out src/main/java/com/todocli/*.java src/main/java/com/todocli/cli/*.java src/main/java/com/todocli/db/*.java src/main/java/com/todocli/model/*.java src/main/java/com/todocli/repository/*.java src/main/java/com/todocli/service/*.java src/main/java/com/todocli/util/*.java src/main/java/com/todocli/exception/*.java
+jar cvfm to-do-cli.jar MANIFEST.MF -C out .
 CLS
 ECHO Successfully Compiled
