5989a01a94fc5ab478494ae00965121a1f2273ec
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-28 22:44:16 -0300

    refactor(cli): Implementa injeção de dependência e reduz responsabilidade
    - Classe agora apenas parseia comandos
    - Delega execução e tratamento de erros para classe dedicada
    - Centraliza mensagens de erro no enum ErrorMessage

--- src/main/java/com/todocli/cli/CommandParser.java
+++ src/main/java/com/todocli/cli/CommandParser.java
@@ -1,105 +1,86 @@
 package com.todocli.cli;
 
-import com.todocli.model.Task;
-import com.todocli.service.TaskService;
-import com.todocli.util.Color;
-
-import java.util.List;
+import com.todocli.util.ErrorMessage;
 
 public class CommandParser {
 
-    private static final TaskService service = new TaskService();
-    private static final String help = """
-                            +----------+----------------------------------+-----------------------------------------------+
-                            | Command  | Description                      | Example                                       |
-                            +----------+----------------------------------+-----------------------------------------------+
-                            | create   | Create a new task                | todo-cli create "Market List" "Buy groceries" |
-                            | update   | Update an existing task by ID    | todo-cli update 1 "To learn" "Learn C"        |
-                            | delete   | Remove a task                    | todo-cli delete 1                             |
-                            | search   | Search a task by keyword         | todo-cli find 1                               |
-                            | find     | Find a task by ID                | todo-cli find 1                               |
-                            | list     | List all tasks                   | todo-cli list                                 |
-                            | list     | List tasks by status             | todo-cli list "todo"                          |
-                            | mark     | Mark the status of a task        | todo-cli mark 1 "done"                        |
-                            +----------+----------------------------------+-----------------------------------------------+
-                            """;
+    private final Commands commands;
 
-    public static void execute(String[] args) {
+    public CommandParser(Commands commands) {
+        this.commands = commands;
+    }
+
+    public void parser(String[] args) {
         if (args.length == 0) {
-            IO.println(help);
+            commands.help();
             return;
         }
+
         switch (args[0]) {
             case "create":
-                if (args.length == 3) {
-                    int id = service.create(args[1], args[2]);
-                    CliConsole.println(Color.getGREEN()
-                            + "Task created successfully (ID: " + id + ")"
-                            + Color.getRESET()
-                    );
+                if (args.length > 1) {
+                    commands.create(args);
                 }
                 else {
-                    CliConsole.printErr("Bad syntax. Use the help command to see available commands.\nTry: create <\"title\"> <\"description\">");
+                    CliConsole.printErr(ErrorMessage.BAD_SYNTAX.getMessage());
                 }
                 break;
             case "delete":
-                if (args.length == 2) {
-                    service.delete(Integer.parseInt(args[1]));
+                if (args.length > 1) {
+                    commands.delete(args);
                 }
                 else {
-                    CliConsole.printErr("Bad syntax. Use the help command to see available commands.\nTry: delete <ID>");
+                    CliConsole.printErr(ErrorMessage.BAD_SYNTAX.getMessage());
                 }
                 break;
             case "update":
-                if (args.length == 4) {
-                    service.update(Integer.parseInt(args[1]), args[2], args[3]);
+                if (args.length > 1) {
+                    commands.update(args);
                 }
                 else {
-                    CliConsole.printErr("Bad syntax. Use the help command to see available commands.\nTry: update <ID> <\"new title\"> <\"new description\">");
+                    CliConsole.printErr(ErrorMessage.BAD_SYNTAX.getMessage());
                 }
                 break;
             case "mark":
-                if (args.length == 3) {
-                    service.mark(Integer.parseInt(args[1]), args[2]);
+                if (args.length > 1) {
+                    commands.mark(args);
                 }
                 else {
-                    CliConsole.printErr("Bad syntax. Use the help command to see available commands.\nTry: mark <ID> <\"status (todo, done, in progress)\">");
+                    CliConsole.printErr(ErrorMessage.INVALID_ID.getMessage());
                 }
                 break;
             case "find":
-                if (args.length == 2) {
-                    Task task = service.find(Integer.parseInt(args[1]));
-                    CliConsole.println(task);
+                if (args.length > 1) {
+                    commands.find(args);
                 }
                 else {
-                    CliConsole.printErr("Bad syntax. Use the help command to see available commands.\nTry: find <ID>");
+                    CliConsole.printErr(ErrorMessage.BAD_SYNTAX.getMessage());
                 }
                 break;
             case "list":
-                if (args.length == 1) {
-                    List<Task> tasks = service.findAll();
-                    CliConsole.printTasks(tasks);
-                }
-                else if (args.length == 2){
-                    List<Task> tasksByStatus = service.findByStatus(args[1]);
-                    CliConsole.printTasks(tasksByStatus);
+                if (args.length >= 1) {
+                    commands.list(args);
                 }
                 else {
-                    CliConsole.printErr("Bad syntax. Use the help command to see available commands.");
+                    CliConsole.printErr(ErrorMessage.BAD_SYNTAX.getMessage());
                 }
                 break;
             case "search":
-                if (args.length == 2) {
-                    List<Task> tasksBySearch = service.searchByTitle(args[1]);
-                    CliConsole.printTasks(tasksBySearch);
+                if (args.length > 1) {
+                    commands.search(args);
                 }
                 else {
-                    CliConsole.printErr("Bad syntax. Use the help command to see available commands.\nTry: search <\"keyword\">");
+                    CliConsole.printErr(ErrorMessage.BAD_SYNTAX.getMessage());
                 }
                 break;
             case "help":
                 if (args.length == 1) {
-                    IO.println(help);
+                    commands.help();
+                }
+                break;
+            default:
+                if (args.length == 1) {
+                    CliConsole.printErr(ErrorMessage.INVALID_COMMAND);
                 }
                 break;
         }
