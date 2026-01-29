b5f597b77356481f6eb179cf6566e97a9b108a76
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-29 20:34:48 -0300

    refactor(main): Adiciona modificador public ao método main
    - Melhora compatibilidade com JVMs mais antigas

--- src/main/java/com/todocli/Main.java
+++ src/main/java/com/todocli/Main.java
@@ -3,13 +3,14 @@ package com.todocli;
 import com.todocli.cli.CliConsole;
 import com.todocli.cli.CommandExecutor;
 import com.todocli.cli.CommandParser;
+import com.todocli.exception.DbException;
 import com.todocli.exception.InvalidArgumentException;
 import com.todocli.exception.ServiceException;
 import com.todocli.repository.SQLiteRepository;
 import com.todocli.service.TaskService;
 
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             SQLiteRepository sqLiteRepository = new SQLiteRepository();
             TaskService taskService = new TaskService(sqLiteRepository);
@@ -17,7 +18,7 @@ public class Main {
             CommandParser commandParser = new CommandParser(commandExecutor);
             commandParser.parser(args);
         }
-        catch (ServiceException | InvalidArgumentException e) {
+        catch (ServiceException | InvalidArgumentException | DbException e) {
             CliConsole.printErr(e.getMessage());
         }
     }
